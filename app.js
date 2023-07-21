ab95cf43531db0f9ae906db5b6793e71e46630aa
Manuel <neycariom@gmail.com>
2023-07-21 02:17:54 +0100

    CORS habilitado e passagem de parâmetros no body.

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false })); //Significa que vou aceitar apenas dados simples
+app.use(bodyParser.json()); //Significa que vou aceitar apenas dados no formato json como entrada
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+
+    next();
+});
+
+/*
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://servidor-especifico.com')
+});
+*/
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
